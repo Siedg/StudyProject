8dcb71fd4296ea7f66c2d728cf68bc509d086e8d
guilherme.lima <guilherme.lima@brmalls.com.br>
2024-11-13 02:05:16 -0300

    Added TrimBinarySearchTree

---
+++ app/src/main/java/br/com/studyproject/exercises/TrimBinarySearchTree.kt
@@ -0,0 +1,22 @@
+package br.com.studyproject.exercises
+
+import br.com.studyproject.model.TreeNode
+
+class TrimBinarySearchTree {
+    fun trimBST(root: TreeNode?, low: Int, high: Int): TreeNode? {
+        if (root == null) return null
+
+        if (root.`val` < low) {
+            return trimBST(root.right, low, high)
+        }
+
+        if (root.`val` > high) {
+            return trimBST(root.left, low, high)
+        }
+
+        root.left = trimBST(root.left, low, high)
+        root.right = trimBST(root.right, low, high)
+
+        return root
+    }
+}
