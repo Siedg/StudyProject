5b0f4fd5ac9dbe3087a4e68e2954b21ae66d1510
guilherme.lima <guilherme.lima@brmalls.com.br>
2024-11-14 01:18:27 -0300

    Added Gepetto

---
+++ app/src/main/java/br/com/studyproject/model/Gepetto.kt
@@ -0,0 +1,34 @@
+package br.com.studyproject.model
+
+class Gepetto {
+    fun binarySearch(list: List<Int>, element: Int): Boolean {
+        if (list.isEmpty()) return false
+
+        var left = 0
+        var right = list.lastIndex
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+
+            when {
+                list[mid] > element -> right = mid - 1
+                (list[mid] < element) -> left = mid + 1
+                list[mid] == element -> return true
+            }
+        }
+
+        return false
+    }
+
+    fun countCharsInArrayWithHashMap(s: String): String? {
+        if (s.isEmpty()) return null
+
+        val hashMap: HashMap<Char, Int> = hashMapOf()
+
+        s.forEach {
+            hashMap[it] = hashMap.getOrDefault(it, 0) + 1
+        }
+
+        return hashMap.toString()
+    }
+}
