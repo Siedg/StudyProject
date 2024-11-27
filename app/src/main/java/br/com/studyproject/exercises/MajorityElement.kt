aa79c9a319c11a0ca26a41a32b2bdddabe1d7e55
guilherme.lima <guilherme.lima@brmalls.com.br>
2024-11-27 00:23:05 -0300

    Added majorityElement

---
+++ app/src/main/java/br/com/studyproject/exercises/MajorityElement.kt
@@ -0,0 +1,15 @@
+package br.com.studyproject.exercises
+
+fun majorityElement(nums: IntArray): Int {
+    var candidate = nums[0]
+    var count = 0
+
+    for (num in nums) {
+        if (count == 0) {
+            candidate = num
+        }
+        count += if (num == candidate) 1 else -1
+    }
+
+    return candidate
+}
