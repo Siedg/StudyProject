8d0fbdac4064a57c4802ed93f6b299a915cf52a1
guilherme.lima <guilherme.lima@brmalls.com.br>
2024-11-27 20:32:36 -0300

    Added jump

--- app/src/main/java/br/com/studyproject/exercises/JumpGame.kt
+++ app/src/main/java/br/com/studyproject/exercises/JumpGame.kt
@@ -17,3 +17,20 @@ fun canJump(nums: IntArray): Boolean {
 
     return false
 }
+
+fun jump(nums: IntArray): Int {
+    var jumps = 0
+    var current_end = 0
+    var farthest = 0
+
+    for (i in 0 until nums.size - 1) {
+        farthest = maxOf(farthest, i + nums[i])
+
+        if (i == current_end) {
+            jumps++
+            current_end = farthest
+        }
+    }
+
+    return jumps
+}
